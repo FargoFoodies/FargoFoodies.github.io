d915e15515f7d192a966de95f73931656b75f468
Ujjwal Adhikari <ujjwalnd@Ujjwals-MacBook-Pro.local>
2023-02-06 14:16:24 -0600

    Made carosel

--- app.js
+++ app.js
@@ -35,6 +35,8 @@ const navSlide = () => {
       
     })
 }
+
+// Contact Form
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
@@ -53,6 +55,28 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+
+// Carosel
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
 
 const app = () =>{
     navSlide()
